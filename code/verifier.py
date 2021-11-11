0dccb7c1fbcd059f315b8da8f7b6a2167c587c45
Fiona Hefti <heftif@student.ethz.ch>
2021-11-11 17:25:14 +0100

    Extracted Network Information from net and constructed a dummyNet

--- code/verifier.py
+++ code/verifier.py
@@ -1,21 +1,94 @@
 import argparse
 import torch
+import torchvision
+import numpy as np
 from networks import FullyConnected
+from networks import Normalization
+from networks import SPU
 from utilities import *
 
 DEVICE = 'cpu'
 INPUT_SIZE = 28
 
+class dummyLayers:
+    def __init__(self):
+        self.type = []
+        self.lowerBounds = []
+        self.upperBounds = []
+        self.operations = []
+
+class operations:
+    def __init__(self, layer, inputFormat, outputFormat, mean=0, std=0, weights=[], bias=[]):
+        self.layer= layer
+        self.inputFormat = inputFormat
+        self.outputFormat = outputFormat
+        self.mean = mean
+        self.std = std
+        self.weights = weights
+        self.bias = bias
+
+def normalize(x):
+    mean = 0.1307
+    sigma = 0.3081
+    return (x-mean)/sigma
+
 
 def analyze(net, inputs, eps, true_label):
-    # denormalize
+    pixels = 784
+
+    #normalize input the data (here? or after -eps/+eps? or both?
+    inputs = normalize(inputs)
 
     # compute lower and upper bounds
+    #inputs-eps does not set values smaller than zero -> is that correct? Should that be the case?
     l = inputs - eps
     u = inputs + eps
 
-    # run inputs through net (with backprop)
-    # how to determine single layers, i.e. transformations?
+    #do we need to normalize the input again?
+
+    #cut lower and upper bounds at 0 and 1, as the task states we have to verify only for possible input
+    l[l<0] = 0
+    u[u>1] = 1
+
+    #extract weights and biases from linear layers
+    l = net.state_dict()
+
+    #initialise object arrays
+    operation = []
+    #read the network
+    #get all the layers with their corresponding in and out features
+    for i in range(0, len(net.layers)):
+        layer = net.layers[i]
+        if isinstance(layer, Normalization):
+            operation.append(operations('Normalization', 784, 784, layer.mean.item(), layer.sigma.item()))
+        elif isinstance(layer, SPU):
+            operation.append(operations('SPU', operation[i-2].outputFormat, operation[i-2].outputFormat))
+        elif isinstance(layer, torch.nn.Linear):
+            #extracts the weights and bias of the layers
+            operation.append(operations('Linear', layer.in_features, layer.out_features,
+                                        weights =l['layers.'+ str(i) + '.weight'], bias = l['layers.'+ str(i) + '.bias']))
+        #print(layer)
+        #what about the flatten layer?
+        #maybe think about setting the predecessor
+
+
+    #create a dummy network object, where we can push the data through
+    dummyNetwork = dummyLayers()
+    dummyNetwork.upperBounds = u
+    dummyNetwork.lowerBounds = l
+    dummyNetwork.operations = operation
+
+    #ERAN Code: look at line 1400 in main -> go to eran analyze box -> line 95 deep poly -> follow the functions
+    #for the actual analization of the given network, similar to what I set up until here, go to analyzer.analyze
+    #in eran -> get_abstract0 -> transformer -> deeppoly_nodes.py -> calc_bounds
+
+    #see deeppoly_nodes.py -> calc_bounds for the next steps taht need to be implemented.
+
+    #labels = np.linspace(0,9,num =10)
+    #compare all labels with all adversarial labels
+    #for label in labels:
+        #for adv_label in labels:
+
 
     # check that proba(true_label) > proba(any other label)
 
@@ -56,6 +129,8 @@ def main():
     outs = net(inputs)
     pred_label = outs.max(dim=1)[1].item()
     assert pred_label == true_label
+
+
 
     if analyze(net, inputs, eps, true_label):
         print('verified')
